f32fc38756406a953d7e92e3f929983bf0b68ceb
Thiluxsan <thiluxsan1995@gmail.com>
2018-01-09 15:48:08 +0530

    pushed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,21 @@ import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { MaterialModule } from './material.module';
+import { MaterialModule } from './material/material.module';
+import { AppRoutingModule } from './/app-routing.module';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeroesComponent,
   ],
   imports: [
     BrowserModule,
     MaterialModule,
     BrowserAnimationsModule,
     NoopAnimationsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
